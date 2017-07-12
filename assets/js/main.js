3832aedac5652d567d8da54f9bc4ba950d6e4af0
Bryan Tran <bryanvantran@ucla.edu>
2017-07-11 23:14:57 -0700

    everything but the quote carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
-$('#video-play').click(function() {
-    console.log('clicked!');
+// play video
+$('#video-play').modalVideo({channel:'vimeo'});
+
+// initialize carousel
+$('.carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
