dcd89e53af1f68e5260d3adfc949c450dee53d6a
Bryan Tran <bryanvantran@ucla.edu>
2017-11-21 20:50:00 -0800

    update podcast link, minify images, fixed slider, and fade in

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,59 @@
-// navbar responsive
-$('.navbar__icon').click(function() {
-    $('.navbar').toggleClass('navbar--responsive');
-});
+$(function() {
+    $('body').removeClass('fade-out');
 
-// book cal button
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y > 100) {
-    $('.navbar--book').fadeIn();
-  } else {
-    $('.navbar--book').fadeOut();
-  }
-});
-
-setInterval(slideCarousel, 5000);
-
-// play video
-$('#video-play').modalVideo({channel:'vimeo'});
-$('#video-play-youtube').modalVideo();
-
-function slideCarousel() {
-    $('.carousel').children().each(function(){
-        var classes = $(this).attr('class');
-        var index = $(this).data('position');
-        var nextIndex = ++index > 5 ? 1 : index;
-
-       
-        if (nextIndex === 1) {
-           $(this).attr('class', 'quote quote--1')
-        } else {
-            $(this).addClass('quote--'+nextIndex);
-        }
-
-        $(this).data('position', nextIndex);
+    // navbar responsive
+    $('.navbar__icon').click(function() {
+        $('.navbar').toggleClass('navbar--responsive');
     });
-}
 
-// for slider
-jQuery(document).ready(function ($) {
-    setInterval(function () {
+    // book cal button
+    $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 100) {
+        $('.navbar--book').fadeIn();
+    } else {
+        $('.navbar--book').fadeOut();
+    }
+    });
+
+    // play video
+    $('#video-play').modalVideo({channel:'vimeo'});
+    $('#video-play-youtube').modalVideo();
+
+    $('.logo-quote').click(function(e) {
+        $('#quote').html($(this).data('quote'));
+        $('#author').html($(this).data('author'));
+    });
+
+    $('#submit_form').click(function(e) {
+        if (!$('#id_firstname').val()) {
+            return;
+        }
+        if (!$('#id_email').val()) {
+            return;
+        }
+        if (!ValidateEmail($('#id_email').val())) {
+            return;
+        }
+        $('#awf_field-92395624').val($('#id_firstname').val());
+        $('#awf_field-92395625').val($('#id_email').val());
+        $('#signup-form').submit();
+    });
+
+    $('.testimonials__thumbnails__element').click(function(e) {
+        $('.testimonials__main__note.active').hide().removeClass('active');
+        $('#'+$(this).data('id')).show().addClass('active');
+    });
+
+    function ValidateEmail(email) {  
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {  
+            return true;
+        }  
+        return false;  
+    } 
+
+
+    var sliderTimer = setInterval(function () {
         moveRight();
     }, 3000);
   
@@ -60,6 +75,10 @@ jQuery(document).ready(function ($) {
             $('.photo-carousel ul li:last-child').prependTo('.photo-carousel ul');
             $('.photo-carousel ul').css('left', '');
         });
+        clearInterval(sliderTimer);
+        sliderTimer = setInterval(function () {
+            moveRight();
+        }, 3000);
     };
 
     function moveRight() {
@@ -69,6 +88,10 @@ jQuery(document).ready(function ($) {
             $('.photo-carousel ul li:first-child').appendTo('.photo-carousel ul');
             $('.photo-carousel ul').css('left', '');
         });
+        clearInterval(sliderTimer);
+        sliderTimer = setInterval(function () {
+            moveRight();
+        }, 3000);
     };
 
     $('.photo-carousel__nav--left').click(function () {
@@ -79,36 +102,3 @@ jQuery(document).ready(function ($) {
         moveRight();
     });
 });
-
-$('.logo-quote').click(function(e) {
-    $('#quote').html($(this).data('quote'));
-    $('#author').html($(this).data('author'));
-});
-
-$('#submit_form').click(function(e) {
-    if (!$('#id_firstname').val()) {
-        return;
-    }
-    if (!$('#id_email').val()) {
-        return;
-    }
-    if (!ValidateEmail($('#id_email').val())) {
-        return;
-    }
-    console.log('no errors');
-    $('#awf_field-92395624').val($('#id_firstname').val());
-    $('#awf_field-92395625').val($('#id_email').val());
-    $('#signup-form').submit();
-});
-
-$('.testimonials__thumbnails__element').click(function(e) {
-    $('.testimonials__main__note.active').hide().removeClass('active');
-    $('#'+$(this).data('id')).show().addClass('active');
-});
-
-function ValidateEmail(email) {  
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {  
-        return true;
-    }  
-    return false;  
-} 
